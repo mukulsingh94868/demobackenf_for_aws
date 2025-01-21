366c779bd4727611a356d24a01466b5fb2361f9e
mukulsingh94868 <mukulsingh94868@gmail.com>
2025-01-21 23:08:01 +0530

    changes

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/aws", (req, res) => {
+  res.send("Hello, AWS EC2!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
